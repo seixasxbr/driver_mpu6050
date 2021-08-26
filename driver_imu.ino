f4f68b9cdab6f5859f75addc23d7bd2de14c5307
seixasxbr <80434858+seixasxbr@users.noreply.github.com>
2021-08-26 17:41:15 -0300

    add ros integration

--- driver_imu.ino
+++ driver_imu.ino
@@ -5,15 +5,41 @@
 //Carrega a biblioteca Wire
 #include<Wire.h>
 
+#include <ros.h>
+#include <std_msgs/Float32.h>
+#include <Arduino.h>
+
+ros::NodeHandle nh;
+std_msgs::Float32 Ax;
+ros::Publisher pub_Ax("Ax", &Ax);
+
+std_msgs::Float32 Ay;
+ros::Publisher pub_Ay("Ay", &Ay);
+
+std_msgs::Float32 Az;
+std_msgs::Float32 Gx;
+std_msgs::Float32 Gy;
+std_msgs::Float32 Gz;
+
+
+ros::Publisher pub_Az("Az", &Az);
+ros::Publisher pub_Gx("Gx", &Gx);
+ros::Publisher pub_Gy("Gy", &Gy);
+ros::Publisher pub_Gz("Gz", &Gz);
+
 //Endereco I2C do MPU6050
 const int MPU=0x68;  
 //Variaveis para armazenar valores dos sensores
 float AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+const long PERIODO = 500;
+unsigned long tempoAnterior = 0;
+
 void setup()
 {
-  Serial.begin(9600);
-  //Inicializa o LCD
-//  lcd.begin(20, 4);
+//  Serial.begin(9600);
+//  nh.getHardware()->setBaud(57600);
+  
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B); 
@@ -21,6 +47,14 @@ void setup()
   //Inicializa o MPU-6050
   Wire.write(0); 
   Wire.endTransmission(true);
+  nh.getHardware()->setBaud(57600);
+  nh.initNode();
+  nh.advertise(pub_Ax);
+  nh.advertise(pub_Ay);
+  nh.advertise(pub_Az);
+  nh.advertise(pub_Gx);
+  nh.advertise(pub_Gy);
+  nh.advertise(pub_Gz);
     
 }
 void loop()
@@ -40,7 +74,7 @@ void loop()
   GyZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
   
   //Envia os valores do acelerometro e do giroscópio para o serial sem conversão
-//  Serial.print("AcX = "); Serial.print(AcX);
+//  Serial.print("AcX = ")ros; Serial.print(AcX);
 //  Serial.print(" | AcY = "); Serial.print(AcY);
 //  Serial.print(" | AcZ = "); Serial.print(AcZ);
 //  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
@@ -49,15 +83,35 @@ void loop()
 //  Serial.print(" | GyZ = "); Serial.println(GyZ);
   
   //Envia os valores do acelerometro e do giroscópio para o serial com conversão
-  Serial.print("AcX = "); Serial.print(AcX/16384*9.80665);
-  Serial.print(" | AcY = "); Serial.print(AcY/16384*9.80665);
-  Serial.print(" | AcZ = "); Serial.print(AcZ/16384*9.80665);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
-  Serial.print(" | GyX = "); Serial.print(GyX/131*0.0174533);
-  Serial.print(" | GyY = "); Serial.print(GyY/131*0.0174533);
-  Serial.print(" | GyZ = "); Serial.println(GyZ/131*0.0174533);
-
+//  Serial.print("AcX = "); Serial.print(AcX/16384*9.80665);
+//  Serial.print(" | AcY = "); Serial.print(AcY/16384*9.80665);
+//  Serial.print(" | AcZ = "); Serial.print(AcZ/16384*9.80665);
+//  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
+//  Serial.print(" | GyX = "); Serial.print(GyX/131*0.0174533);
+//  Serial.print(" | GyY = "); Serial.print(GyY/131*0.0174533);
+//  Serial.print(" | GyZ = "); Serial.println(GyZ/131*0.0174533);
+ 
+  unsigned long tempoAtual = millis();
   
+  if (tempoAtual - tempoAnterior >= PERIODO) {
+    tempoAnterior = tempoAtual;
+    Ax.data = AcX/16384*9.80665;  
+    pub_Ax.publish(&Ax);
+    Ay.data = AcY/16384*9.80665; 
+    pub_Ay.publish(&Ay);
+    Az.data = AcZ/16384*9.80665; 
+    Gx.data = GyX/131*0.0174533;  
+    Gy.data = GyY/131*0.0174533; 
+    Gz.data = GyZ/131*0.0174533;
+
+    
+    pub_Az.publish(&Az);
+    pub_Gx.publish(&Gx);
+    pub_Gy.publish(&Gy);
+    pub_Gz.publish(&Gz);
+    } 
+
+  nh.spinOnce();
   //Aguarda 300 ms e reinicia o processo
-  delay(300);
+  //delay(300);
 }
